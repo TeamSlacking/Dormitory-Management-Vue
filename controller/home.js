271ddc4451ffeaf0410d64c2cc52b8ffceee3174
Pink Champagne <r2kou@outlook.com>
2021-10-14 10:13:25 +0800

    fix #48778823 实现首页的组件切换

--- controller/home.js
+++ controller/home.js
@@ -1,15 +1,41 @@
 import { AdminManagement } from "../components/admin-management.js";
 import { IndexWelcome } from "../components/index-welcome.js";
 
+/** @type {Array<{name: string, component: Object}>} */
+const menu = [
+    {
+        name: "主页",
+        component: IndexWelcome,
+    },
+    {
+        name: "宿舍管理员管理",
+        component: AdminManagement,
+    },
+    {
+        name: "学生管理",
+    },
+    {
+        name: "宿舍楼管理",
+    },
+    {
+        name: "缺勤记录",
+    },
+    {
+        name: "修改密码",
+    },
+];
+
 const app = new Vue({
     el: "#app",
-    components: {
-        IndexWelcome,
-        AdminManagement,
-    },
-    data() {
-        return {};
-    },
+    data: () => ({
+        menu,
+        viewName: "主页",
+    }),
     methods: {},
-    computed: {},
+    computed: {
+        viewComponent() {
+            const item = menu.find((item) => item.name === this.viewName);
+            return item.component ?? IndexWelcome;
+        },
+    },
 });
